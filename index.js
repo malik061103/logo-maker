425879b4c06adb129c05601a67a115495a391df1
malik061103 <malikkerek08@gmail.com>
2024-08-25 16:59:48 -0400

    coding

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
-// TODO: Create a constructor function called 'BlogPost' that takes in 'authorName', 'title', 'text, and 'createdOn'
-class BlogPost {
-    constructor(authorName, title, text, createdOn) {
-        this.authorName = authorName;
-        this.title = title;
-        this.text = text;
-        this.createdOn = createdOn;
-        this.printmatadata = function () {
-            console.log(`created by ${this.authorName}on${this.createdOn}`);
+const inquirer = require("inquirer")
+const Svg = require("./lib/svg")
+const { Circle, Triangle, Square } = require("./lib/shapes")
+const fs = require("fs")
+inquirer.prompt([
+    {
+        type: "list",
+        name: "shapes",
+        message: "What's your shape for SVG",
+        choices: ["Triangle", "Circle", "Square"]
+    },
 
-        }
+    {
+        type: "list",
+        name: "shapescolor",
+        message: "What's your shapecolor for SVG",
+        choices: ["red", "blue", "green", "black", "white", "purple", "pink"]
+    },
+
+    {
+        type: "input",
+        name: "text",
+        message: "What's your text for SVG",
+
+    },
+
+    {
+        type: "list",
+        name: "textcolor",
+        message: "What's your textcolor for SVG",
+        choices: ["red", "blue", "green", "black", "white", "purple", "pink"]
+    },
+
+]).then(answers => {
+    const userSvg = new Svg(answers.textcolor)
+    userSvg.setText(answers.text)
+    switch (answers.shapes) {
+        case "Triangle":
+            let userShape1 = new Triangle(answers.shapescolor)
+            userSvg.setShape(userShape1.render());
+            break;
+        case "Circle":
+            let userShape2 = new Circle(answers.shapescolor)
+            userSvg.setShape(userShape2.render());
+            break;
+        case "Square":
+            let userShape3 = new Square(answers.shapescolor)
+            userSvg.setShape(userShape3.render());
+            break;
     }
-
-
-
-
-
-
-
-}
-    // TODO: Include a method called 'printMetaData()' that prints a message in the console saying 'Created by (authorName) on (createdOn)'
-
-    // TODO: Create a new object using the 'BlogPost' constructor
-    const  mypost = new BlogPost("malik","JS is powerful","js is one of the most userful tool you can use for coding","08/25/2024")
-// TODO: Call the 'printMetaData()' method on the new object
-console.log(mypost);
-mypost.printmatadata();
+    fs.writeFile("logo.svg",userSvg.render(),function(err){
+        if(err) throw err;
+    })
+})
